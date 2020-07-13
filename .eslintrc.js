291537eace3c1c1017576d79768a778775770668
Kent Seneres <kseneres@medrhythms.com>
2020-07-13 11:10:32 -0400

    update lint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,9 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  rules: {
+    'no-unreachable': 'warn',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['warn'],
+  },
 };
