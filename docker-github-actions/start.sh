f2b51d459ca419b77cfabd4567ed39cdd1317309
Kent <kent.seneres@gmail.com>
2022-03-21 08:40:26 -0400

    add self hosted runner docker config

---
+++ docker-github-actions/start.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# https://testdriven.io/blog/github-actions-docker/
+
+PROJECT=$PROJECT
+ACCESS_TOKEN=$ACCESS_TOKEN
+
+cd /home/docker/actions-runner
+
+./config.sh --url https://github.com/${PROJECT} --token ${ACCESS_TOKEN}
+
+cleanup() {
+    echo "Removing runner..."
+    ./config.sh remove --unattended --token ${ACCESS_TOKEN}
+}
+
+trap 'cleanup; exit 130' INT
+trap 'cleanup; exit 143' TERM
+
+./run.sh & wait $!
+
