1ff31b17883326642cbccfd0b46cce62524894c1
Kent Seneres <kent.seneres@gmail.com>
2024-10-26 09:20:39 -0400

    Update onecall api 3.0 and react native (#29)
    * use onecall 3.0 api
    * update react-native to v0.75
    * update workflow

--- src/core/constants.ts
+++ src/core/constants.ts
@@ -1,4 +1,4 @@
 export const USE_MOCK_LOCATION = __DEV__ && true;
-export const USE_MOCK_WEATHER_DATA = __DEV__ && true;
+export const USE_MOCK_WEATHER_DATA = __DEV__ && false;
 
 export const SHOW_DETAILS_DURATION_MS = 3000;
